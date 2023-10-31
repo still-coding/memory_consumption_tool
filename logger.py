01961fedcb45f48f3188e0b24a1bf0113ba8b4ea
still-coding <get_lost@whatever.com>
2023-10-31 12:02:17 +0500

    cleanup

--- logger.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-logger = logging.getLogger(__name__)
-
-# set level to logging.WARNING to log sending alarms
-logger.setLevel(level=logging.WARNING)
-file_handler = logging.FileHandler("mem_cons_tool.log", mode='w')
-logger.addHandler(file_handler)
